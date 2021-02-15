c8502f169abb40de516f9d08605237f48cd1e1d0
Bunks <busk_1@yahoo.com>
2021-02-15 17:57:20 -0500

    imported NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Containers/Home";
+import NavBar from "./Components//NavBar/NavBar";
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
+        <NavBar />
         <Wrapper>
           <Route exact path="/" component={Home} />
         </Wrapper>
