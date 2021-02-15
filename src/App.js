0a7b0df30ae2b964b3a94b800c13dddd9372640f
Bunks <busk_1@yahoo.com>
2021-02-15 18:00:42 -0500

    added fixed css link and import

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Containers/Home";
 import NavBar from "./Components//NavBar/NavBar";
+import Wrapper from "./Components/Wrapper/Wrapper";
 
 function App() {
   return (
