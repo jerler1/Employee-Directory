62ae8cdd963d4ab0241416deca2f5fef4d523651
Bunks <busk_1@yahoo.com>
2021-02-15 16:38:10 -0500

    init files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
