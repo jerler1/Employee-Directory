10df3cf358061b2653aa90fa3fdff133ff032f14
Bunks <busk_1@yahoo.com>
2021-02-15 18:54:57 -0500

    API calls are functioning

--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import API from "../util/API";
+// import Container
+// import Employee
+// import EmployeeRow
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  state = {
+    employeeList: {},
+  };
+
+  // When this component is functioning - go get some users.
+  componentDidMount() {
+    API.populatePeople().then((resultingData) => {
+        this.setState({ employeeList: resultingData })
+    });
+  }
+  render() {
+    return (
+        <>
+        // Container
+        // Table Headers
+        // Map over data - which return Row
+        {this.state.employeeList.data?.results.map(result => {
+            return <Row />
+        })}
+        <div>
+            Hi {this.state.employeeList.data?.results[0].name.first}
+        </div>
+        </>);
+  }
 }
 
 export default Home;
